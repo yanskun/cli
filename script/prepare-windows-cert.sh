2a8468ff751c519f24341df241afc43abafb90f9
nate smith <vilmibm@github.com>
2022-01-12 15:27:36 -0600

    theoretically make hook parallelizable

---
+++ script/prepare-windows-cert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl \
+  -H "Authorization: token $DESKTOP_CERT_TOKEN" \
+  -H "Accept: application/vnd.github.v3.raw" \
+  --output windows-certificate.pfx \
+  https://api.github.com/repos/desktop/desktop-secrets/contents/windows-certificate.pfx
+
+openssl pkcs12 -in windows-certificate.pfx -nocerts -nodes -out private-key.pem  -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 1"
+openssl pkcs12 -in windows-certificate.pfx -nokeys -nodes -out certificate.pem -passin pass:${GITHUB_CERT_PASSWORD} || echo "no bueno 2"
